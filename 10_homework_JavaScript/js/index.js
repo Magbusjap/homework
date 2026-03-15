7cb898f3e8080231533d09955e19fae16eec4d44
Michael Ankudinov <43526147+Magbusjap@users.noreply.github.com>
2026-03-15 15:37:21 +0500

    10: add JavaScript basics tasks

---
+++ 10_homework_JavaScript/js/index.js
@@ -0,0 +1,55 @@
+// Задача 1.
+// Создайте переменные с корректными именами, чтобы сохранить следующую информацию:
+// 1. firstName – имя (строка)
+// 2. lastName – фамилия (строка)
+// 3. isStudent – является ли учеником курса (булево значение)
+
+// Задача 2.
+// 1. Объявите переменную age с числовым значением возраста студента.
+// 2. Объявите переменную currentYear и присвойте ей значение текущего года (например, 2025).
+// 3. Используя age и currentYear вычислите год рождения студента, запишите результат в переменную birthYear.
+
+// Задача 3.
+// Выведите в консоли сообщение в таком формате:
+// Меня зовут [firstName] [lastName], мне [age] лет. Я ученик/ученица курса: [isStudent].
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+console.log("Решение задачи 1:");
+
+let firstName = "Иван";
+let lastName = "Иванов";
+const isStudent = true;
+
+console.log(
+	`Его зовут ${firstName} ${lastName}. Он являюсь ${isStudent ? "учеником" : "не учеником"} курса.`,
+);
+
+console.log("Решение задачи 2:");
+
+const age = 18;
+const currentYear = 2025;
+const birthYear = currentYear - age;
+console.log(
+	`Студенту — ${age}лет, на же дворе — ${currentYear}. Следовательно, год рождения студента — ${birthYear}`,
+);
+
+console.log("Решение задачи 3:");
+
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+
+console.log(
+	`result переменная или ${result} является строковой, тип string. (let result: string)`,
+);
